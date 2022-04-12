bf907b65ba982a65dc9888e92a23f9c25e282e91
HectorVillavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2022-04-12 16:56:00 -0300

    cambie a speed
    arregle position poq no habia puesto a speed

--- entities/Player.gd
+++ entities/Player.gd
@@ -14,4 +14,4 @@ func _process(delta):
 	var direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 #otra manera mas bonita
 
-	position.x += direction * 50 * delta
+	position.x += direction * speed * delta
